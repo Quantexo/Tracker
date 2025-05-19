b4c0ca39aa1a957620bdd7c0e829a2376972538b
Quantexo <quantexo2005@gmail.com>
2025-05-19 21:42:19 +0545

    Add files via upload

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ import io
 from datetime import datetime, timedelta
 import plotly.io as pio
 
+st.set_page_config(page_title="Quantexo", layout="wide")
+
+with open("styles.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+st.markdown(
+    """ <style>
+    .stApp {
+    background-color: darkslategray;
+    } </style> <div class='header-container'> <div class='header-title'>Quantexo🕵️</div> <div class='header-subtitle'>💰 Advanced Insights for Bold Trades</div> </div>
+    """,
+    unsafe_allow_html=True
+)
