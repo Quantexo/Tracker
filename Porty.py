9f835a84220a9bff3ebed57b1194c279d32ea2f0
Quantexo <quantexo2005@gmail.com>
2025-05-22 20:00:01 +0545

    Add files via upload

--- Porty.py
+++ Porty.py
@@ -30,8 +30,8 @@ def main():
     st.sidebar.header("Public Google Sheet Setup")
     sheet_id = st.sidebar.text_input("Google Sheet ID (from URL)", help="Get this from the URL: https://docs.google.com/spreadsheets/d/**[ID]**/edit")
     holdings_gid = st.sidebar.text_input("Holdings GID", value="0")
-    transactions_gid = st.sidebar.text_input("Transactions GID", value="1531896722")
-    
+    transactions_gid = st.sidebar.text_input("Transactions GID", value="1347762871")
+
     if sheet_id and holdings_gid and transactions_gid:
         try:
             holdings_url = get_csv_url(sheet_id, holdings_gid)
